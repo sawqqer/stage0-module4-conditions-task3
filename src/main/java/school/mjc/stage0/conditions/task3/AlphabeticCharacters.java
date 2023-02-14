55639ed245a22b9ccb37d5097eb810e39f0321d4
Arman <gcom0733@gmail.com>
2023-02-14 18:04:00 +0600

    implemented methods

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!Character.isLetter(character)) {
+            System.out.println("wrong alphabet!");
+        } else if ("AEIOUaeiou".indexOf(character) != -1) {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            double taxRate = 0.15;
+            double taxAmount = salary * taxRate;
+            double netSalary = salary - taxAmount;
+            System.out.println(netSalary);
+        } else if (salary <= 20000) {
+            double taxRate = 0.18;
+            double taxAmount = salary * taxRate;
+            double netSalary = salary - taxAmount;
+            System.out.println(netSalary);
+        } else {
+            double taxRate = 0.20;
+            double taxAmount = salary * taxRate;
+            double netSalary = salary - taxAmount;
+            System.out.println(netSalary);
+        }
     }
 }
